778d979b1572f44d48ed1a5f321ef34def595d9e
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-08 16:45:31 -0700

    completed react lag

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,17 @@
 class Homepage extends React.Component {
 	render(){
-		return "This component needs to be implemented.";
+		return (
+            <div>
+                <p>Welcome to this world, i hope you get to survive</p> 
+                <a href="/cards"> Cards </a>
+                <br/>
+                <img src="/static/img/balloonicorn.jpg"/>
+            </div>
+        );
 	}
 }
 
 ReactDOM.render(<Homepage />, document.getElementById('app'));
+
+
+
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,7 +4,7 @@ class TradingCard extends React.Component {
 			<div className="card">
 				<h2>Name: {this.props.name}</h2>
 				<img src={this.props.imgUrl} />
-				<h2>Skill: </h2>
+				<h2>Skill:{this.props.skill}</h2>
 			</div>
 		);
 	}
@@ -23,4 +23,14 @@ ReactDOM.render(
 ReactDOM.render(
 	<TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
 	document.getElementById('llambda')
+);
+
+ReactDOM.render(
+	<TradingCard name="wormy" skill="wiggle wiggle" imgUrl="/static/img/images.jpg" />,
+	document.getElementById('wormy')
+);
+
+ReactDOM.render(
+	<TradingCard name = "poly" skill ="change" imgUrl="/static/img/polymorphism.jpg"/>,
+	document.getElementById("poly")
 );
